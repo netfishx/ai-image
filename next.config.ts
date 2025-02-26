ec58d5be1c06c0d6770b307fed6f8993e1622fe2
cricket <your_email@example.com>
2025-02-26 10:54:07 +0900

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "X-Accel-Buffering",
+            value: "no",
+          },
+        ],
+      },
+    ];
+  },
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? {
+            exclude: ["error", "dir", "group", "groupEnd"],
+          }
+        : false,
+  },
+  devIndicators: {
+    position: "bottom-right",
+  },
+  experimental: {
+    ppr: true,
+    reactCompiler: true,
+    dynamicIO: true,
+    authInterrupts: true,
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+    useCache: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
