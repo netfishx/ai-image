bd5be045fdca806880ca5add68d27820cae58d5a
cricket <your_email@example.com>
2025-03-05 13:37:33 +0900

    init

--- src/app/(games)/video/form.tsx
+++ src/app/(games)/video/form.tsx
@@ -67,7 +67,7 @@ export function VideoForm() {
         请上传原视频
       </div>
       <div>
-        <Upload type="video" name="video" />
+        <Upload type="video" name="video" maxSize={200} />
       </div>
       <div className="flex justify-center px-8">
         <Button type="submit" className="w-full" disabled={isPending}>
