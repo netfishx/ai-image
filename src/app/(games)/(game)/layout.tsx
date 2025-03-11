3574bef3ab8db9e39d7be078edb57b9e9a6c19e0
cricket <your_email@example.com>
2025-03-11 11:21:30 +0900

    init

--- src/app/(games)/(game)/layout.tsx
+++ src/app/(games)/(game)/layout.tsx
@@ -1,12 +1,26 @@
 import { CustomMarquee } from "@/app/(games)/(game)/custom-marquee";
 import { CustomTabs } from "@/app/(games)/(game)/custom-tabs";
+import ad from "@/assets/ad.gif";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { rgbDataURL } from "@/lib/utils";
+import Image from "next/image";
 import type { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <CustomTabs />
+      <div className="relative h-25">
+        <Image
+          src={ad}
+          alt="ad"
+          fill
+          placeholder="blur"
+          blurDataURL={rgbDataURL(200, 200, 200)}
+          className="rounded-lg object-cover"
+          sizes="100vw"
+        />
+      </div>
       <CustomMarquee />
       <ScrollArea className="h-[calc(100dvh-100px)]">{children}</ScrollArea>
     </>
