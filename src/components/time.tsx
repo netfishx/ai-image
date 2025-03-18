1354383d861cf78fbb848715fb871b3e3b69dc7f
cricket <your_email@example.com>
2025-03-18 13:39:05 +0900

    init

--- src/components/time.tsx
+++ src/components/time.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
 
 export function Time({ time }: { time: string }) {
-  const [date, setDate] = useState("");
-  useEffect(() => {
-    setDate(format(time, "yyyy-MM-dd HH:mm:ss"));
-  }, [time]);
-  return <span>{date}</span>;
+  return <span>{format(time, "yyyy-MM-dd HH:mm:ss")}</span>;
 }
